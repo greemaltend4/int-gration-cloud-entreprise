1dd115e891bc6d57bf6deb0ced637467befca668
greemaltend4 <dengestabiy@hotmail.com>
2025-11-03 10:47:29 +0000

    feat: initial commit for config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// Configuration des paramètres d'intégration
+
+module.exports = {
+  salesforce: {
+    clientId: 'VOTRE_CLIENT_ID',
+    clientSecret: 'VOTRE_CLIENT_SECRET',
+    redirectUri: 'VOTRE_URL_DE_REDIRECTION'
+  },
+  googleDrive: {
+    clientId: 'VOTRE_CLIENT_ID',
+    clientSecret: 'VOTRE_CLIENT_SECRET'
+  },
+  // Autres configurations d'intégration ici
+};
